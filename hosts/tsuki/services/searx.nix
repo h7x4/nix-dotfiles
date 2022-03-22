267e6aeacaeafce43e27e4fa0bcf179245e85e29
h7x4 <h7x4abk3g@protonmail.com>
2022-03-22 23:06:02 +0100

    Configure searx

--- hosts/tsuki/services/searx.nix
+++ hosts/tsuki/services/searx.nix
@@ -21,33 +21,134 @@
         theme_args.oscar_style = "logicodev-dark";
       };
      
-      engines = [
-        # {
-        #   name = "YouTube";
-        #   shortcut = "yt";
-        #   engine = "youtube_api";
-        # }
-        {
-          name = "fdroid";
-          engine = "fdroid";
-        }
-        {
-          name = "github";
-          engine = "github";
-        }
-        {
-          name = "ebay";
-          engine = "ebay";
-        }
-        # {
-        #   name = "bandcamp";
-        #   engine = "bandcamp";
-        # }
-        {
-          name = "arch_linux_wiki";
-          shortcut = "aw";
-          engine = "archlinux";
-        }
+      engines = let
+        enable = name: {
+          name = name;
+          disabled = false;
+        };
+        disable = name: {
+          name = name;
+          disabled = true;
+        };
+      in [
+        /* General */
+        (disable "bing")
+        /* --- */
+        (enable "archive is")
+        (enable "wikipedia")
+        (enable "currency")
+        (enable "ddg definitions")
+        (enable "erowid")
+        (enable "wikidata")
+        (enable "duckduckgo")
+        (enable "etymonline")
+        (enable "google")
+        (enable "library genesis")
+        (enable "qwant")
+        (enable "reddit")
+        (enable "wikibooks")
+        (enable "wikiquote")
+        (enable "wikisource")
+        (enable "wiktionary")
+        (enable "wikiversity")
+        # Doesn't work at the time
+        # (enable "wikivoyage")
+        (enable "dictzone")
+
+        /* Files */
+        # (disable "btdigg")
+        (disable "piratebay")
+        (disable "torrentz")
+        /* --- */
+        (enable "apk mirror")
+        (enable "fdroid")
+        (enable "google play apps")
+        (enable "nyaa")
+        (enable "tokyotoshokan")
+
+        /* Images */
+        (disable "bing images")
+        (disable "ccengine")
+        (disable "flickr")
+        (disable "library of congress")
+        /* --- */
+        (enable "deviantart")
+        (enable "google images")
+        (enable "nyaa")
+        # (enable "reddit")
+        (enable "qwant images")
+        (enable "unsplash")
+
+        /* IT */
+        (enable "arch linux wiki")
+        (enable "bitbucket")
+        (enable "free software directory")
+        (enable "gentoo")
+        (enable "gitlab")
+        (enable "github")
+        (enable "codeberg")
+        (enable "hoogle")
+        (enable "npm")
+        (enable "stackoverflow")
+        (enable "searchcode code")
+
+        /* map */
+        (enable "openstreetmap")
+        (enable "photon")
+
+        /* music */
+        (disable "btdigg")
+        /* --- */
+        (enable "deezer")
+        (enable "genius")
+        (enable "mixcloud")
+        # (enable "nyaa")
+        (enable "soundcloud")
+        # (enable "tokyotoshokan")
+        (enable "youtube")
+
+        /* news */
+        (disable "bing news")
+        (disable "digg")
+        (disable "yahoo news")
+        /* --- */
+        (enable "google news")
+        (enable "qwant news")
+        # (enable "reddit")
+        (enable "wikinews")
+
+        /* science */
+        (enable "arxiv")
+        (enable "crossref")
+        (enable "google scholar")
+        (enable "microsoft academic")
+        (enable "openairedatasets")
+        (enable "openairepublications")
+        (enable "pdbe")
+        (enable "pubmed")
+        (enable "semantic scholar")
+        (enable "wolframalpha")
+
+        /* social media */
+        # (disable "digg")
+        # (enable "reddit")
+
+        /* shopping */
+        # (enable "ebay")
+
+        /* videos */
+        (disable "bing videos")
+        # (disable "piratebay")
+        (disable "sepiasearch")
+        (disable "dailymotion")
+        (disable "mediathekviewweb")
+        /* --- */
+        (enable "google videos")
+        # (enable "nyaa")
+        # (enable "tokyotoshokan")
+        # (enable "youtube")
+        (enable "vimeo")
+        (enable "peertube")
       ];
     };
 
