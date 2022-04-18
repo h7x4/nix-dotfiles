f49bd969d298f55738edc60c1c2b8358d6eb5ae3
h7x4 <h7x4abk3g@protonmail.com>
2022-04-18 05:50:06 +0200

    Add gitmirror to pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,17 @@
 { pkgs ? import <nixpkgs> {} }: let
   call = pkg: { name = pkg; value = pkgs.callPackage ./${pkg} {}; };
 in builtins.listToAttrs (map call [
-  # TODO: Add some packages
+  "gitmirror"
+  # "koneko"
+  # "listen-moe"
+  # "bb"
 ])
+
+# {
+#   koneko = pkgs.callPackage ./koneko {};
+#   listen-moe = pkgs.callPackage ./listen-moe {};
+#   simplicity-studio = pkgs.callPackage ./simplicity-studio-5 {};
+#   # deezloader-remix = pkgs.callPackage ./pkgs/deezloader-remix {};
+#   ani-cli = pkgs.callPackage ./ani-cli {};
+#   bb = pkgs.callPackage ./bb {};
+# }
