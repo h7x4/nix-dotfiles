fe3bdd49496298240c5c13470f1f960e7180b966
h7x4 <h7x4@nani.wtf>
2022-09-22 01:58:26 +0200

    add 'https' to hydra link for proper website links

--- hosts/tsuki/services/hydra.nix
+++ hosts/tsuki/services/hydra.nix
@@ -4,7 +4,7 @@
   # https://gist.github.com/joepie91/c26f01a787af87a96f967219234a8723
   services.hydra = {
     enable = true;
-    hydraURL = "http://hydra.nani.wtf";
+    hydraURL = "https://hydra.nani.wtf";
     notificationSender = "hydra@nani.wtf";
     useSubstitutes = true;
     package = unstable-pkgs.hydra_unstable;
