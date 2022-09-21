2a722dbf7347836284b81b32a14e5f849dafda25
h7x4 <h7x4@nani.wtf>
2022-09-22 00:51:00 +0200

    Use hydra with unstable nix tool

--- hosts/tsuki/services/hydra.nix
+++ hosts/tsuki/services/hydra.nix
@@ -1,4 +1,4 @@
-{ secrets, ... }:
+{ pkgs, unstable-pkgs, secrets, ... }:
 {
   # Follow instructions for setup:
   # https://gist.github.com/joepie91/c26f01a787af87a96f967219234a8723
@@ -7,6 +7,8 @@
     hydraURL = "http://hydra.nani.wtf";
     notificationSender = "hydra@nani.wtf";
     useSubstitutes = true;
+    package = unstable-pkgs.hydra_unstable;
     port = secrets.ports.hydra;
+    buildMachinesFiles = [];
   };
 }
