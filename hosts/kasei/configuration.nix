6b037127e2e931ae327554753b2cb990f187b7b0
h7x4 <h7x4@nani.wtf>
2023-02-25 18:00:02 +0100

    kasei: add temporary logid service

--- hosts/kasei/configuration.nix
+++ hosts/kasei/configuration.nix
@@ -45,6 +45,23 @@
 
   # security.pam.services.login.unixAuth = true;
 
+  # TODO: remove when merged: https://github.com/NixOS/nixpkgs/pull/167388
+  systemd.services.logid = {
+    description = "Logitech Configuration Daemon";
+    startLimitIntervalSec = 0;
+    wants = [ "multi-user.target" ];
+    after = [ "multi-user.target" ];
+    wantedBy = [ "graphical-session.target" ];
+
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.logiops}/bin/logid";
+      User = "root";
+      ExecReload = "/bin/kill -HUP $MAINPID";
+      Restart="on-failure";
+    };
+  };
+
   boot = {
     blacklistedKernelModules = ["nouveau"];
     kernelParams = ["nomodeset"];
