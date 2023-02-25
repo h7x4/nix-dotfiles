8b1df87747d77891a0866cb2930b340602bc35cb
h7x4 <h7x4@nani.wtf>
2023-02-25 17:56:00 +0100

    firefox: set up bookmarks

--- home/programs/firefox.nix
+++ home/programs/firefox.nix
@@ -1,8 +1,12 @@
-{ ... }:
+{ secrets, ... }:
 {
   programs.firefox = {
     enable = true;
     profiles.h7x4 = {
+      bookmarks = [{
+        toolbar = true;
+        bookmarks = secrets.browser.bookmarks;
+      }];
       settings = {};
     };
   };
