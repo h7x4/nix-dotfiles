72ec7c3f2cac36064421cb10ef4ab7a26feab51c
h7x4 <h7x4@nani.wtf>
2023-03-07 23:03:04 +0100

    gtk: make gtk3 bookmarks public
    Let's hope they won't cause a CVE or something

--- home/config/gtk.nix
+++ home/config/gtk.nix
@@ -14,5 +14,16 @@
       name = "VimixDark";
     };
     gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+
+    gtk3.bookmarks = map (s: "file://${config.home.homeDirectory}/${s}") [
+      "Dropbox"
+      "Downloads"
+      "git/pvv"
+      "nix"
+      "NTNU"
+      "ng"
+      "git"
+      "music"
+    ];
   };
 }
