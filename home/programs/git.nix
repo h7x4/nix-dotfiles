10997b802cb3c3a1bd313032e4597bfcc5d43f28
h7x4 <h7x4@nani.wtf>
2023-05-08 01:49:22 +0200

    home/git: add `authors` alias

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -27,6 +27,7 @@
         graph = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
         graphv = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
         forcepush = "push --force-with-lease --force-if-includes";
+        authors = "shortlog --summary --numbered --email";
     };
 
     extraConfig = {
