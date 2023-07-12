c8db83b925a3470298428b7963a96b4a9eea8151
h7x4 <h7x4@nani.wtf>
2023-07-12 02:00:06 +0200

    tsuki/plex: harden systemd unit

--- hosts/tsuki/services/plex.nix
+++ hosts/tsuki/services/plex.nix
@@ -1,9 +1,33 @@
-{ config, secrets, ... }:
-{
+{ config, secrets, ... }: let 
+  cfg = config.services.plex;
+in {
   services.plex = {
     enable = true;
     openFirewall = true;
     dataDir = "${config.machineVars.dataDrives.default}/var/plex";
+  };
+
+  systemd.services.plex.serviceConfig = {
+    ReadWritePaths = [ cfg.dataDir ];
+    NoNewPrivileges = true;
+    PrivateDevices = true;
+    ProtectClock = true;
+    ProtectKernelLogs = true;
+    ProtectKernelModules = true;
+    PrivateMounts = true;
+    RestrictSUIDSGID = true;
+    ProtectHostname = true;
+    LockPersonality = true;
+    ProtectKernelTunables = true;
+    ProtectSystem = "strict";
+    ProtectProc = true;
+    ProtectHome = true;
+    # PrivateNetwork = true;
+    PrivateUsers = true;
+    PrivateTmp = true;
+    UMask = "0007";
+    # RestrictAddressFamilies = [ "AF_UNIX AF_INET AF_INET6" ];
+    SystemCallArchitectures = "native";
   };
 
   # networking.firewall.allowedTCPPorts = [ secrets.ports.plex ];
