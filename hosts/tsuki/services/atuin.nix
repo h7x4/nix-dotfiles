6663a8f280c3d77ff8fbe5e252813ebc1600973b
h7x4 <h7x4@nani.wtf>
2023-07-28 22:25:50 +0200

    tsuki/atuin: systemd harden

--- hosts/tsuki/services/atuin.nix
+++ hosts/tsuki/services/atuin.nix
@@ -4,4 +4,42 @@
     enable = true;
     openRegistration = false;
   };
+
+  systemd.services.atuin = {
+    requires = [ "postgresql.service" ];
+    serviceConfig = {
+      # Hardening
+      CapabilityBoundingSet = "";
+      LockPersonality = true;
+      NoNewPrivileges = true;
+      PrivateDevices = true;
+      PrivateMounts = true;
+      PrivateTmp = true;
+      PrivateUsers = true;
+      ProcSubset = "pid";
+      ProtectClock = true;
+      ProtectControlGroups = true;
+      ProtectHome = true;
+      ProtectHostname = true;
+      ProtectKernelLogs = true;
+      ProtectKernelModules = true;
+      ProtectKernelTunables = true;
+      ProtectProc = "invisible";
+      ProtectSystem = "full";
+      RemoveIPC = true;
+      RestrictAddressFamilies = [
+        "AF_INET"
+        "AF_INET6"
+        # Required for connecting to database sockets,
+        # and listening to unix socket at `cfg.settings.path`
+        "AF_UNIX"
+      ];
+      RestrictNamespaces = true;
+      RestrictRealtime = true;
+      RestrictSUIDSGID = true;
+      SystemCallArchitectures = "native";
+      SystemCallFilter = "~@clock @cpu-emulation @debug @keyring @module @mount @obsolete @raw-io @reboot @setuid @swap @privileged";
+      UMask = "0007";
+    };
+  };
 }
