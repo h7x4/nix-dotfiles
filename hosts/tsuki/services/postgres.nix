72e7626e9d31ac2407856f181d380886b1282157
h7x4 <h7x4@nani.wtf>
2024-01-23 05:31:06 +0100

    tsuki/postgres: tune for bare metal setup

--- hosts/tsuki/services/postgres.nix
+++ hosts/tsuki/services/postgres.nix
@@ -11,7 +11,31 @@ in {
       host all all ::1/128 trust
     '';
     settings = {
+      # Source: https://pgtune.leopard.in.ua/
+      # DB Version: 15
+      # OS Type: linux
+      # DB Type: mixed
+      # Total Memory (RAM): 12 GB
+      # CPUs num: 12
+      # Connections num: 150
+      # Data Storage: hdd
+      
       max_connections = 150;
+      shared_buffers = "3GB";
+      effective_cache_size = "9GB";
+      maintenance_work_mem = "768MB";
+      checkpoint_completion_target = 0.9;
+      wal_buffers = "16MB";
+      default_statistics_target = 100;
+      random_page_cost = 4;
+      effective_io_concurrency = 2;
+      work_mem = "2621kB";
+      min_wal_size = "1GB";
+      max_wal_size = "4GB";
+      max_worker_processes = 12;
+      max_parallel_workers_per_gather = 4;
+      max_parallel_workers = 12;
+      max_parallel_maintenance_workers = 4;
     };
   };
 
