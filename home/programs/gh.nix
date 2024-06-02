13201436dac5066c929a530bf418c0d8add19e5c
h7x4 <h7x4@nani.wtf>
2024-06-02 16:24:05 +0200

    home/gh: set pager to bat

--- home/programs/gh.nix
+++ home/programs/gh.nix
@@ -1,9 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.gh = {
     enable = true;
     settings = {
       gitProtocol = "ssh";
+      pager = "${pkgs.bat}/git/bat";
       aliases = {
         co = "pr checkout";
         pv = "pr view";
