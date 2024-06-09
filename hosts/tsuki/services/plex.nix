53c6c32fb8c3300a46c35274367ed8012e937b7b
h7x4 <h7x4@nani.wtf>
2024-06-10 00:45:19 +0200

    tsuki/plex: remove security hardening, included in nixos 24.05

--- hosts/tsuki/services/plex.nix
+++ hosts/tsuki/services/plex.nix
@@ -5,27 +5,4 @@ in {
     enable = true;
     openFirewall = true;
   };
-
-  systemd.services.plex.serviceConfig = {
-    ReadWritePaths = [ cfg.dataDir ];
-    NoNewPrivileges = true;
-    PrivateDevices = true;
-    ProtectClock = true;
-    ProtectKernelLogs = true;
-    ProtectKernelModules = true;
-    PrivateMounts = true;
-    RestrictSUIDSGID = true;
-    ProtectHostname = true;
-    LockPersonality = true;
-    ProtectKernelTunables = true;
-    ProtectSystem = "strict";
-    ProtectProc = true;
-    ProtectHome = true;
-    # PrivateNetwork = true;
-    PrivateUsers = true;
-    PrivateTmp = true;
-    UMask = "0007";
-    # RestrictAddressFamilies = [ "AF_UNIX AF_INET AF_INET6" ];
-    SystemCallArchitectures = "native";
-  };
 }
