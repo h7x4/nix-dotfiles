4869f550266054776e5e9ab1101e7156de7d4db6
h7x4 <h7x4@nani.wtf>
2024-07-09 11:02:27 +0200

    dosei/home: proxyjump pvv through wstunnel

--- hosts/dosei/home/default.nix
+++ hosts/dosei/home/default.nix
@@ -1,4 +1,14 @@
 { ... }:
 {
   home.stateVersion = "24.05";
+
+  programs.ssh.matchBlocks = {
+    "tsuki-ws" = {
+      user = "h7x4";
+      hostname = "localhost";
+      port = 10022;
+    };
+
+    "hildring pvv-login pvv".proxyJump = "tsuki-ws";
+  };
 }
