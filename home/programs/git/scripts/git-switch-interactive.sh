881aaedd4ad1158228233fd876a9af34bcce8311
h7x4 <h7x4@nani.wtf>
2024-07-09 11:05:25 +0200

    home/git/switch-interactive: fix for branchnames with '/'

--- home/programs/git/scripts/git-switch-interactive.sh
+++ home/programs/git/scripts/git-switch-interactive.sh
@@ -7,5 +7,10 @@ fi
 
 BRANCHES=$(cat <(git branch) <(git branch --remotes) | grep --invert-match '^\*\|HEAD ->' | sed 's|^\s*||')
 CHOSEN_BRANCH=$(fzf --reverse --info=inline --preview 'git show --color {}' <<<"$BRANCHES")
-CLEAN_BRANCH_NAME=$(sed 's|^\s*.*/||' <<<"$CHOSEN_BRANCH")
-git switch "$CLEAN_BRANCH_NAME"
+
+CLEAN_BRANCH_NAME=$(sed 's|^\s*||' <<<"$CHOSEN_BRANCH")
+for REMOTE in $(git remote); do
+  CLEAN_BRANCH_NAME=$(sed "s|^${REMOTE}/||" <<<"$CLEAN_BRANCH_NAME")
+done
+
+git switch "${CLEAN_BRANCH_NAME}"
