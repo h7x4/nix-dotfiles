f3850074b47874968fe21c22d11ce28b22bbbaea
h7x4 <h7x4@nani.wtf>
2024-08-12 17:39:08 +0200

    home/xdg: lowercase more userdirs

--- home/config/xdg/default.nix
+++ home/config/xdg/default.nix
@@ -9,13 +9,13 @@
     userDirs = {
       enable = true;
       desktop = "${config.home.homeDirectory}/Desktop";
-      documents = "${config.home.homeDirectory}/Documents";
+      documents = "${config.home.homeDirectory}/documents";
       download = "${config.home.homeDirectory}/Downloads";
       music = "${config.home.homeDirectory}/music";
-      pictures = "${config.home.homeDirectory}/Pictures";
+      pictures = "${config.home.homeDirectory}/pictures";
       publicShare = "${config.home.homeDirectory}/public";
       templates = "${config.home.homeDirectory}/templates";
-      videos = "${config.home.homeDirectory}/Videos";
+      videos = "${config.home.homeDirectory}/videos";
     };
   };
 }
