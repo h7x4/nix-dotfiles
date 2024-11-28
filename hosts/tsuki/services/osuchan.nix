edf81976ac82e0667d47b450376e8f37706f3219
h7x4 <h7x4@nani.wtf>
2024-11-29 00:17:35 +0100

    tsuki/osuchan: use sops template for secrets

--- hosts/tsuki/services/osuchan.nix
+++ hosts/tsuki/services/osuchan.nix
@@ -1,12 +1,26 @@
 { config, ... }:
 {
-  sops.secrets."osuchan/envfile" = {
-    restartUnits = [ "osuchan.service" ];
+  sops = {
+    secrets = {
+      "osuchan/env/channel_access_token" = { };
+      "osuchan/env/channel_id" = { };
+      "osuchan/env/channel_secret" = { };
+    };
+    templates."osuchan.env" = {
+      restartUnits = [ "osuchan.service" ];
+      content = let
+        inherit (config.sops) placeholder;
+      in ''
+        CHANNEL_ACCESS_TOKEN=${placeholder."osuchan/env/channel_access_token"}
+        CHANNEL_ID=${placeholder."osuchan/env/channel_id"}
+        CHANNEL_SECRET=${placeholder."osuchan/env/channel_secret"}
+      '';
+    };
   };
 
   services.osuchan = {
     enable = true;
     port = 9283;
-    secretFile = config.sops.secrets."osuchan/envfile".path;
+    secretFile = config.sops.templates."osuchan.env".path;
   };
 }
