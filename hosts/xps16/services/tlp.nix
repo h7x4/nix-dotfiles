bc3e2e648f0b97e773b4602cb07dde0964601c76
h7x4 <h7x4@nani.wtf>
2024-12-08 18:30:58 +0100

    xps16/tlp: add some recommended settings from arch wiki

--- hosts/xps16/services/tlp.nix
+++ hosts/xps16/services/tlp.nix
@@ -1,4 +1,11 @@
 { ... }:
 {
-  services.tlp.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      RUNTIME_PM_ON_AC = "auto";
+      RUNTIME_PM_ON_BAT = "auto";
+      USB_AUTOSUSPEND = "0";
+    };
+  };
 }
