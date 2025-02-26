5a168a6e5d4a089ea71bfea66f917c092032fa33
h7x4 <h7x4@nani.wtf>
2025-02-26 10:20:16 +0100

    kasei/postgres: bump from 14 -> 17

--- hosts/kasei/services/postgres.nix
+++ hosts/kasei/services/postgres.nix
@@ -1,8 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.postgresql = {
     enable = true;
     enableTCPIP = true;
+    package = pkgs.postgresql_17;
     authentication = ''
       # tailscale xps
       host all all 100.94.170.21/32 md5
