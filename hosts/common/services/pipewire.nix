a1a98fc5800f007013372d16f5a4959da3debaad
h7x4 <h7x4@nani.wtf>
2025-03-04 13:39:04 +0100

    common/pipewire: fix

--- hosts/common/services/pipewire.nix
+++ hosts/common/services/pipewire.nix
@@ -14,11 +14,12 @@
           "bluez5.enable-sbc-xq" = true;
           "bluez5.enable-msbc" = true;
           "bluez5.enable-hw-volume" = true;
-          "bluez5.roles" = [
-            "hsp_hs"
-            "hsp_ag"
-            "hfp_hf"
-            "hfp_ag"
+          "bluez5.codecs" = [
+            "ldac"
+            "aac"
+            "sbc_xq"
+            "aptx"
+            "sbc"
           ];
         };
       };
