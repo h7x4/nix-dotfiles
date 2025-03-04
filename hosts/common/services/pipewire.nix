c1dcf35d630cc8be0712362c25dbdb9f7ed04771
h7x4 <h7x4@nani.wtf>
2025-03-04 13:05:51 +0100

    common/pipewire: enable wireplumber, add bluetooth config

--- hosts/common/services/pipewire.nix
+++ hosts/common/services/pipewire.nix
@@ -5,5 +5,23 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
+    wireplumber = {
+      enable = true;
+
+      extraConfig."10-bluez" = {
+        "monitor.bluez.properties" = {
+          "bluez5.enable-sbc-xq" = true;
+          "bluez5.enable-msbc" = true;
+          "bluez5.enable-hw-volume" = true;
+          "bluez5.roles" = [
+            "hsp_hs"
+            "hsp_ag"
+            "hfp_hf"
+            "hfp_ag"
+          ];
+        };
+      };
+    };
   };
 }
