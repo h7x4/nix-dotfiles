ccc72027c52c609906ae96729d4a8ff34976a024
h7x4 <h7x4@nani.wtf>
2025-03-13 16:23:46 +0100

    tsuki/ssh: set Macs

--- hosts/tsuki/configuration.nix
+++ hosts/tsuki/configuration.nix
@@ -58,6 +58,12 @@
   };
 
   services.openssh.enable = true;
+  services.openssh.settings.Macs = [
+    "hmac-sha2-512-etm@openssh.com"
+    "hmac-sha2-256-etm@openssh.com"
+    "umac-128-etm@openssh.com"
+    "hmac-sha2-512"
+  ];
 
   systemd.services."sshd@".serviceConfig = {
     Nice = -15;
