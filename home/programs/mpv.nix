2d3e7f8825307c49a9d93838fba07a786c2e3d34
h7x4 <h7x4@nani.wtf>
2025-03-24 11:12:11 +0100

    home/mpv: misc config

--- home/programs/mpv.nix
+++ home/programs/mpv.nix
@@ -1,4 +1,16 @@
-{ machineVars, ... }:
+{ config, lib, machineVars, ... }:
 {
-  programs.mpv.enable = !machineVars.headless;
+  programs.mpv = {
+    enable = !machineVars.headless;
+
+    config = {
+      screenshot-directory = "${config.xdg.userDirs.pictures}/mpv-screenshots";
+
+      #https://wiki.nixos.org/wiki/Accelerated_Video_Playback
+      hwdec       = "auto-safe";
+      vo          = "gpu";
+      profile     = "gpu-hq";
+      gpu-context = lib.mkIf machineVars.wayland "wayland";
+    };
+  };
 }
