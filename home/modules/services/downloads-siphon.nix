a04c13b8d946149deabf1c50ad2858b9e0492341
h7x4 <h7x4@nani.wtf>
2025-03-24 14:55:33 +0100

    home/modules: add wip module downloads-siphon

---
+++ home/modules/services/downloads-siphon.nix
@@ -0,0 +1,9 @@
+{ ... }:
+{
+  # TODO: create thingamajig that watches ~/Downloads and pulls all new files to
+  #       directory specified by systemd %i
+
+  # systemd.services.
+
+  # systemd.paths
+}
