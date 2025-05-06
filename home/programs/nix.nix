150089a583823e1d641495d1330fec209c57db5c
h7x4 <h7x4@nani.wtf>
2025-05-06 13:05:11 +0200

    {common,home}/nix: finegrained tokens

--- home/programs/nix.nix
+++ home/programs/nix.nix
@@ -1,15 +1,32 @@
-{ config, ... }:
+{ config, lib, ... }:
 {
   sops = {
     secrets = {
-      "nix/access-tokens/github" = { sopsFile = ../../secrets/common.yaml; };
-      "nix/access-tokens/pvv-git" = { sopsFile = ../../secrets/common.yaml; };
+      "nix/access-tokens/github" = { sopsFile = ./../../secrets/common.yaml; };
+
+      "nix/access-tokens/pvv-git" = { sopsFile = ./../../secrets/common.yaml; };
+
+      "nix/access-tokens/github-nordicsemi" = { sopsFile = ./../../secrets/common.yaml; };
+      "nix/access-tokens/bitbucket-nordicsemi" = { sopsFile = ./../../secrets/common.yaml; };
     };
     templates."nix-access-tokens.conf".content = let
       inherit (config.sops) placeholder;
-    in ''
-      access-tokens = github.com=${placeholder."nix/access-tokens/github"} git.pvv.ntnu.no=${placeholder."nix/access-tokens/pvv-git"}
-    '';
+
+      tokens = {
+        "github.com" = placeholder."nix/access-tokens/github";
+
+        "git.pvv.ntnu.no" = placeholder."nix/access-tokens/pvv-git";
+
+        "bitbucket.nordicsemi.no" = placeholder."nix/access-tokens/bitbucket-nordicsemi";
+        "github.com/NordicPlayground" = placeholder."nix/access-tokens/github-nordicsemi";
+        "github.com/NordicSemiconductor" = placeholder."nix/access-tokens/github-nordicsemi";
+      };
+    in "access-tokens = ${lib.pipe tokens [
+      lib.attrsToList
+      (builtins.sort (p: q: p.name > q.name))
+      (map ({ name, value }: "${name}=${value}"))
+      (builtins.concatStringsSep " ")
+    ]}";
   };
 
   nix = {
--- hosts/common/nix.nix
+++ hosts/common/nix.nix
@@ -1,4 +1,4 @@
-{ config, unstable-pkgs, ... }:
+{ config, lib, unstable-pkgs, ... }:
 {
   imports = [
     ./nix-builders/bob.nix
@@ -9,13 +9,30 @@
   sops = {
     secrets = {
       "nix/access-tokens/github" = { sopsFile = ./../../secrets/common.yaml; };
+
       "nix/access-tokens/pvv-git" = { sopsFile = ./../../secrets/common.yaml; };
+
+      "nix/access-tokens/github-nordicsemi" = { sopsFile = ./../../secrets/common.yaml; };
+      "nix/access-tokens/bitbucket-nordicsemi" = { sopsFile = ./../../secrets/common.yaml; };
     };
     templates."nix-access-tokens.conf".content = let
       inherit (config.sops) placeholder;
-    in ''
-      access-tokens = github.com=${placeholder."nix/access-tokens/github"} git.pvv.ntnu.no=${placeholder."nix/access-tokens/pvv-git"}
-    '';
+
+      tokens = {
+        "github.com" = placeholder."nix/access-tokens/github";
+
+        "git.pvv.ntnu.no" = placeholder."nix/access-tokens/pvv-git";
+
+        "bitbucket.nordicsemi.no" = placeholder."nix/access-tokens/bitbucket-nordicsemi";
+        "github.com/NordicPlayground" = placeholder."nix/access-tokens/github-nordicsemi";
+        "github.com/NordicSemiconductor" = placeholder."nix/access-tokens/github-nordicsemi";
+      };
+    in "access-tokens = ${lib.pipe tokens [
+      lib.attrsToList
+      (builtins.sort (p: q: p.name > q.name))
+      (map ({ name, value }: "${name}=${value}"))
+      (builtins.concatStringsSep " ")
+    ]}";
   };
 
   nix = {
