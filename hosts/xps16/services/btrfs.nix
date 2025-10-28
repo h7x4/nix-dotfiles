27ac053b4747353a7ff7ff9c5e099c7dc92a8611
h7x4 <h7x4@nani.wtf>
2025-10-28 14:32:01 +0900

    xps16/btrfs/scrub: disable internet, restrict filesystems

--- hosts/xps16/services/btrfs.nix
+++ hosts/xps16/services/btrfs.nix
@@ -1,4 +1,12 @@
-{ ... }:
+{ utils, ... }:
 {
   services.btrfs.autoScrub.enable = true;
+
+  systemd.services."btrfs-scrub-${utils.escapeSystemdPath "/"}".serviceConfig = {
+    PrivateNetwork = true;
+    RestrictFileSystems = [
+      "sysfs" # Needed to set scrub speed in /sys/fs/btrfs
+      "btrfs"
+    ];
+  };
 }
