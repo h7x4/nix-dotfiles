5a7269f55d5604f6795f8bcc3daed982a3775fe4
h7x4 <h7x4@nani.wtf>
2025-11-05 09:27:37 +0900

    common/irqbalance: apply chroot

--- hosts/common/services/irqbalance.nix
+++ hosts/common/services/irqbalance.nix
@@ -1,4 +1,21 @@
-{ ... }:
+{ config, lib, ... }:
+let
+  cfg = config.services.irqbalance;
+in
 {
   services.irqbalance.enable = true;
+
+  systemd.services.irqbalance.serviceConfig = lib.mkIf cfg.enable {
+    RuntimeDirectory = [
+      "irqbalance"
+      "irqbalance/root-mnt"
+    ];
+
+    RootDirectory = "/run/irqbalance/root-mnt";
+    BindReadOnlyPaths = [
+      builtins.storeDir
+    ];
+    # NoExecPaths = "/";
+    # ExecPaths = lib.getExe cfg.package;
+  };
 }
