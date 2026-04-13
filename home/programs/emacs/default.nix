c7d02db34e00448c7adbf90a00cffb19e45665d7
h7x4 <h7x4@nani.wtf>
2026-04-13 14:26:58 +0900

    home/emacs: fetch a lot of packages directly from nixpkgs

--- home/programs/emacs/default.nix
+++ home/programs/emacs/default.nix
@@ -22,22 +22,57 @@ lib.mkIf cfg.enable {
 
   programs.emacs = {
     extraPackages = epkgs: with epkgs; [
-    #   # package
+      # gruber-darker-theme
+      # recentf
+      centaur-tabs
+      company
+      company-box
+      company-coq
+      counsel
+      dashboard
+      doom-modeline
+      drag-stuff
+      emojify
+      envrc
+      evil
+      evil-collection
+      evil-escape
+      evil-nerd-commenter
+      evil-org
+      fira-code-mode
+      flycheck
+      general
+      git-gutter
+      helpful
+      highlight-defined
+      hl-todo
+      ivy
+      ivy-rich
+      ivy-yasnippet
+      lorem-ipsum
+      lsp-ivy
+      lsp-mode
+      lsp-treemacs
+      lsp-ui
+      magit
+      monokai-theme
+      nix-mode
+      org
+      org-superstar
+      projectile
+      proof-general
+      rainbow-delimiters
+      ripgrep
+      swiper
+      treemacs
+      treemacs-evil
+      treemacs-icons-dired
+      treemacs-magit
+      treemacs-projectile
       use-package
-    #   evil
-    #   evil-collection
-    #   evil-nerd-commenter
-    #   # org
-    #   evil-org
-    #   monokai-theme
-    #   gruber-darker-theme
-    #   company
-    #   flycheck
-    #   projectile
-    #   yasnippet
-    #   magit
-    #   # recentf
-    #   which-key
+      which-key
+      writeroom-mode
+      yasnippet
     ];
   };
 }
