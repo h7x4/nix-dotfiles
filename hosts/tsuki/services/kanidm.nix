e39e9ef32d067af2dc1111bd0c27431032f202f3
h7x4 <h7x4@nani.wtf>
2026-06-05 12:17:21 +0900

    tsuki/kanidm: 1_9 -> 1_10

--- hosts/tsuki/services/kanidm.nix
+++ hosts/tsuki/services/kanidm.nix
@@ -17,7 +17,7 @@ in {
   };
 
   services.kanidm = {
-    package = pkgs.kanidm_1_9;
+    package = pkgs.kanidm_1_10;
     # enablePAM = true;
     server.settings = let
       credsDir = "/run/credentials/kanidm.service";
