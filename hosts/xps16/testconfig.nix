c36832413d1b8040a4daaf4009fbcc3b6418e808
h7x4 <h7x4@nani.wtf>
2026-06-05 12:11:15 +0900

    xps16/testconfig: clear

--- hosts/xps16/testconfig.nix
+++ hosts/xps16/testconfig.nix
@@ -2,5 +2,5 @@
 # NOTE: this file is a space where i put new configuration while i'm testing it.
 #       There shouldn't really be anything here, I'm not planning to commit any config here.
 {
-
+  networking.hosts."129.241.100.145" = [ "scrutiny.pvv.ntnu.no" ];
 }
