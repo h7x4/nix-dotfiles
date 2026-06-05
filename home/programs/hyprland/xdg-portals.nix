eb31d5282aa60b23dc863948ea3e494caee67647
h7x4 <h7x4@nani.wtf>
2026-06-05 11:01:14 +0900

    home/xdg-portals: disable xdg-desktop-portal-termfilechooser, kinda broken with some programs

--- home/programs/hyprland/xdg-portals.nix
+++ home/programs/hyprland/xdg-portals.nix
@@ -9,26 +9,26 @@ in {
       extraPortals = with pkgs; [
         gnome-keyring
         xdg-desktop-portal-gtk
-        xdg-desktop-portal-termfilechooser
+        # xdg-desktop-portal-termfilechooser
       ];
       config.hyprland = {
         default = "hyprland;gtk;";
-        "org.freedesktop.impl.portal.FileChooser" = [ "termfilechooser" ];
+        # "org.freedesktop.impl.portal.FileChooser" = [ "termfilechooser" ];
         "org.freedesktop.impl.portal.OpenURI" = [ "gtk" ];
         "org.freedesktop.impl.portal.Notification" = [ "gtk" ];
         "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
       };
     };
 
-    xdg.configFile."xdg-desktop-portal-termfilechooser/config".source =
-      (pkgs.formats.ini { listsAsDuplicateKeys = true; }).generate "xdg-desktop-portal-termfilechooser.ini" {
-        filechooser = {
-          cmd = "${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh";
-          default_dir = "$HOME";
-          env = [
-            "TERMCMD=alacritty -T \"Filechooser\" --class \"xdg-desktop-portal-termfilechooser\" --command"
-          ];
-        };
-      };
+    # xdg.configFile."xdg-desktop-portal-termfilechooser/config".source =
+    #   (pkgs.formats.ini { listsAsDuplicateKeys = true; }).generate "xdg-desktop-portal-termfilechooser.ini" {
+    #     filechooser = {
+    #       cmd = "${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh";
+    #       default_dir = "$HOME";
+    #       env = [
+    #         "TERMCMD=alacritty -T \"Filechooser\" --class \"xdg-desktop-portal-termfilechooser\" --command"
+    #       ];
+    #     };
+    #   };
   };
 }
