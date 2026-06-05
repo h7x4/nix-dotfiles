e09d9be2330ea4ee6ed759efbb088fe96ae05d15
h7x4 <h7x4@nani.wtf>
2026-06-05 10:56:53 +0900

    overlays/pcloud: fix build

--- overlays/pcloud.nix
+++ overlays/pcloud.nix
@@ -2,19 +2,22 @@
 (_: _: {
   pcloud = pkgs.pcloud.overrideAttrs (prev: {
     buildInputs = prev.buildInputs ++ (with pkgs; [
+      gnome2.GConf
+      libXScrnSaver
+      libXtst
       libappindicator
       libindicator
       libnotify
-      libXScrnSaver
-      libXtst
     ]);
 
     preFixup = ''
-      rm "$out"/lib/libappindicator.so.1
-      rm "$out"/lib/libindicator.so.7
-      rm "$out"/lib/libnotify.so.4
-      rm "$out"/lib/libXss.so.1
-      rm "$out"/lib/libXtst.so.6
+      rm "$out"/lib/libappindicator3.so.1*
+      rm "$out"/lib/libindicator3.so.7*
+      rm "$out"/lib/libnotify.so.4*
+      rm "$out"/lib/libXss.so.1*
+      rm "$out"/lib/libXtst.so.6*
+      rm "$out"/lib/libgconf-2.so.4*
+      rmdir lib
     '';
   });
 })
